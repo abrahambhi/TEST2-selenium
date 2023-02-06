0d65558263a6fb620c451290ee3d64140e6e8ed9
= <=>
2023-02-06 13:04:09 +0530

    firstcommit

--- TEST1/Form1.Designer.cs
+++ TEST1/Form1.Designer.cs
@@ -34,7 +34,7 @@ namespace TEST1
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(267, 40);
+            this.button1.Location = new System.Drawing.Point(272, 35);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(215, 41);
             this.button1.TabIndex = 0;
